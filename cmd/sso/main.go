18bbc114ec3275a1b3f6aebba429470696d9f2a0
Doremi203 <maxim.r.ivanov@gmail.com>
2024-07-16 23:20:42 +0300

    Added main with config and logger

---
+++ cmd/sso/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"authservice/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting server",
+		slog.String("Env", cfg.Env),
+		slog.Int("Port", cfg.Grpc.Port))
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelError,
+			}),
+		)
+	}
+
+	return log
+}
