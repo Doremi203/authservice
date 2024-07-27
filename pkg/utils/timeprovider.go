e6c8d604b19d6feb34b33d7892fb8bc9ea4e5c8f
Doremi203 <maxim.r.ivanov@gmail.com>
2024-07-27 15:30:20 +0300

    Implemented time provider

---
+++ pkg/utils/timeprovider.go
@@ -0,0 +1,18 @@
+package utils
+
+import "time"
+
+type TimeProvider interface {
+	Now() time.Time
+	UTCNow() time.Time
+}
+
+type DefaultTimeProvider struct{}
+
+func (d DefaultTimeProvider) Now() time.Time {
+	return time.Now()
+}
+
+func (d DefaultTimeProvider) UTCNow() time.Time {
+	return time.Now().UTC()
+}
