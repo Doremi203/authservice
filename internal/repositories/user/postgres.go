0a20a141c38c15224e6c709d6141ba3e5d52a12a
Doremi203 <maxim.r.ivanov@gmail.com>
2024-07-29 19:15:18 +0300

    Implemented postgres user repository stub

---
+++ internal/repositories/user/postgres.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"authservice/internal/domain/entities"
+	"authservice/internal/domain/types"
+	"context"
+)
+
+type PostgresRepository struct{}
+
+func NewPostgresRepository() *PostgresRepository {
+	return &PostgresRepository{}
+}
+
+func (p *PostgresRepository) Add(ctx context.Context, userData entities.User) (user entities.User, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresRepository) GetByEmail(ctx context.Context, email types.Email) (user entities.User, err error) {
+	//TODO implement me
+	panic("implement me")
+}
